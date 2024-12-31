dc7a9442d3c313a419a1631db9db394ee678aafe
ShAsHi <shashinarmala29@gmail.com>
2024-12-31 19:47:16 +0530

    Update gactionrunner.sh

--- Projects/gactionrunner.sh
+++ Projects/gactionrunner.sh
@@ -1,7 +1,8 @@
 # Create a folder
 mkdir actions-runner && cd actions-runner
 # Download the latest runner package
-curl -o actions-runner-linux-x64-2.321.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.321.0/actions>echo "ba46ba7ce3a4d7236b16fbe44419fb453bc08f866b24f04d549ec89f1722a29e  actions-runner-linux-x64-2.321.0.tar.gz" | shas># Extract the installer
+curl -o actions-runner-linux-x64-2.321.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.321.0/actions>echo "ba46ba7ce3a4d7236b16fbe44419fb453bc08f866b24f04d549ec89f1722a29e  actions-runner-linux-x64-2.321.0.tar.gz" | shas>
+# Extract the installer
 tar xzf ./actions-runner-linux-x64-2.321.0.tar.gz
 
 echo "Downloaded Ga Actions Runner Successfully"
