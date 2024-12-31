b4fc41721cca0fcacc167703747b1dea43689457
ShAsHi <shashinarmala29@gmail.com>
2024-12-31 20:58:49 +0530

    Update gactionrunner.sh

--- Projects/gactionrunner.sh
+++ Projects/gactionrunner.sh
@@ -1,20 +1,20 @@
 echo "credentials for the runner"
 
 echo "Enter URL of Repository ::"
-read $URLREPO
+read -s  URLREPO
 
 echo "Enter Token ::"
-read $Token
+read -s Token
 
 
 # Create a folder for the GitHub Actions Runner
 mkdir actions-runner && cd actions-runner
 
 # Download the latest runner package
-curl -o actions-runner-linux-x64-2.321.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.321.0/actions-runner-linux-x64-2.321.0.tar.gz
+curl -o actions-runner-linux-x64-2.321.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.321.0/>
 
 # Verify the integrity of the downloaded package
-echo "ba46ba7ce3a4d7236b16fbe44419fb453bc08f866b24f04d549ec89f1722a29e  actions-runner-linux-x64-2.321.0.tar.gz" | shasum -a 256 --check
+echo "ba46ba7ce3a4d7236b16fbe44419fb453bc08f866b24f04d549ec89f1722a29e  actions-runner-linux-x64-2.321.0.tar.gz">
 
 # Extract the installer
 tar xzf ./actions-runner-linux-x64-2.321.0.tar.gz
@@ -25,7 +25,7 @@ echo "Downloaded GitHub Actions Runner successfully."
 sleep 1
 
 # Configure the GitHub Actions Runner
-./config.sh --url $URLREPO --token $Token
+./config.sh --unattended --url $URLREPO --token $Token
 
 # Start the GitHub Actions Runner
 ./run.sh
